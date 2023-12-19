5f871d2a8d7da3fe677cc5c82771a192e7f16315
da1pi2 <danieldpipitone@gmail.com>
2023-12-19 14:35:31 +0100

    Add Docstring to functions

--- main.py
+++ main.py
@@ -7,6 +7,19 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 # Function to read a file and return content
 def read_file(file_path):
+    """
+    Reads the content of a file given its file path.
+
+        Parameters:
+            file_path (str): The path to the file to be read.
+
+        Returns:
+            str: The content of the file as a string.
+
+        Raises:
+            FileNotFoundError: If the file does not exist, prints an 
+                                error message and exits the program.
+    """
     try:
         with open(file_path, encoding="utf-8") as file:
             return file.read()
@@ -16,16 +29,55 @@ def read_file(file_path):
 
 # Function to preprocess text
 def preprocess_text(text, language="english"):
+    """
+    Convert text to lowercase, remove punctuation, and tokenize it.
+    
+    Parameters:
+    - text (str): String to preprocess.
+    - language (str, optional): Language for tokenization. Defaults to
+      "english".
+    
+    Returns:
+    - list: List of tokens from the processed text.
+    """
     text = text.lower().translate(str.maketrans('', '', string.punctuation))
     return word_tokenize(text, language)
 
 # Function to remove stopwords from a list of words
 def filter_stopwords(words, language):
+    """
+    Filters out stop words from a list of words based on the specified
+    language.
+
+    Parameters:
+    words (list of str): The list of words from which to filter out stop
+    words.
+    language (str): The language for which to filter stop words, e.g., 
+    'english'.
+
+    Returns:
+    list of str: A list of words with stop words removed.
+    """
     stop_words = set(stopwords.words(language))
     return [word for word in words if word not in stop_words]
 
 # Function to read and parse emotions from a file
 def parse_emotions(file_path, filtered_words):
+    """
+    Parses a file to extract emotions associated with specific
+    words. Filters out words not in the provided list.
+
+    Parameters:
+    - file_path (str): The path to the file containing word-emotion
+                       pairs on separate lines, formatted as
+                       "word:emotion".
+    - filtered_words (list): List of words to filter the emotions
+                             by.
+
+    Returns:
+    - emotion_list (list): A list of emotions corresponding to the
+                           filtered words found in the file.
+    """
     emotion_list = []
     try:
         with open(file_path, 'r') as file:
@@ -41,6 +93,20 @@ def parse_emotions(file_path, filtered_words):
 
 # Function to perform sentiment analysis
 def sentiment_analyse(text):
+    """
+    Analyzes the sentiment of a given text.
+
+    This function takes a string input, calculates the sentiment
+    score using SentimentIntensityAnalyzer, and prints whether
+    the sentiment is positive, negative, or neutral based on the
+    computed scores.
+
+    Parameters:
+    text (str): The text to analyze sentiment for.
+
+    Returns:
+    None
+    """
     score = SentimentIntensityAnalyzer().polarity_scores(text)
     if score['neg'] > score['pos']:
         print("Negative Sentiment")
@@ -51,6 +117,14 @@ def sentiment_analyse(text):
 
 # Function to get the sentiment of a word
 def get_word_sentiment(word):
+    """
+    Analyzes the sentiment of a given word using sentiment intensity.
+    Parameters:
+        word (str): The word to analyze the sentiment of.
+    Returns:
+        str: 'positive', 'negative', or 'neutral' based on the sentiment
+             score of the word.
+    """
     sentiment_scores = SentimentIntensityAnalyzer().polarity_scores(word)
     if sentiment_scores['compound'] > 0.1:
         return 'positive'
@@ -61,6 +135,15 @@ def get_word_sentiment(word):
 
 # Function to map emotions to colors
 def map_emotions_to_colors(emotion_counts):
+    """
+    Maps emotional sentiments to corresponding colors.
+
+    Args:
+        emotion_counts: A dictionary with emotions as keys and their counts as values.
+
+    Returns:
+        A list of strings representing colors associated with each emotional sentiment.
+    """
     color_map = {'positive': 'green', 'negative': 'red', 'neutral': 'gray'}
     return [color_map[get_word_sentiment(emotion)] for emotion in emotion_counts.keys()]
 
